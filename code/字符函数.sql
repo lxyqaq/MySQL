ef3ddb79eaf3e2a2c8211c4c77af8a345b5e0668
lxyqaq <lxyqaq0128@gmail.com>
2020-09-24 21:19:23 +0100

    字符函数

--- code/字符函数.sql
+++ code/字符函数.sql
@@ -93,5 +93,77 @@ SELECT
 FROM
         employees;
 
+# 4.substr、substring
+# 注意：索引从1开始
+
+# 截取从指定索引处后面所有字符
+
+SELECT
+        substr('李莫愁爱上了陆展元' , 7) AS "out_put";
+
+# 截取从指定索引处指定字符长度的字符
+
+SELECT
+        substr('李莫愁爱上了陆展元' , 1 , 3) AS "out_put";
+
+#案例：姓名中首字符大写，其他字符小写然后用_拼接，显示出来
+
+SELECT
+        concat(upper(substr(last_name , 1 , 1)) , '_' , lower(substr(last_name,2))) AS "姓名"
+FROM
+        employees;
+
+#5.instr 返回子串第一次出现的索引，如果找不到返回0
+
+SELECT
+        instr('杨不悔爱上了殷六侠' , '殷六侠') AS "out_put";
+
+# trim
+
+SELECT
+        trim('     张翠山    ') AS "out_put";
+
+SELECT
+        trim('a' FROM 'aaaaaaa张aaaaaa翠山aaaaaa') AS "out_put";
+
+#7.lpad 用指定的字符实现左填充指定长度
+
+SELECT
+        lpad('殷素素' , 10 , '*') AS "out_put";
+
+SELECT
+        lpad('殷素素' , 1 , '*') AS "out_put";
+
+#7.rpad 用指定的字符实现右填充指定长度
+
+SELECT
+        rpad('殷素素' , 12 , '*') AS "out_put";
+
+SELECT
+        rpad('殷素素' , 1 ,'*') AS "out_put";
+
+#9.replace 替换
+
+SELECT
+        replace('周芷若周芷若周芷若周芷若张无忌爱上了周芷若' , '周芷若' , '赵敏');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
